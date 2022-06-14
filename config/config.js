f598bc62895d3672b3851912c074ac6143c7b535
aaqromrnh <achaqrom05@gmail.com>
2022-06-14 21:11:28 +0700

    update config.js

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
-    password: process.env.CI_DB_PASSWORD,
+    password: process.env.DB_PASSWORD,
     database: 'final_project_db',
     host: '127.0.0.1',
     port: 5432,
