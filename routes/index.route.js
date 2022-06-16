794d398f9c7d142a86e9048b2d9fe9374348ba32
Qoharul <qoharuljabbar@gmail.com>
2022-06-16 21:48:31 +0700

    Done API Product Create & Read

--- routes/index.route.js
+++ routes/index.route.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const authRoutes = require("./auth.route");
+const authProducts = require("./product.route");
 
 router.use("/api/v1/auth", authRoutes);
+router.use("/api/v1/product", authProducts);
 
 module.exports = router;
---
+++ routes/product.route.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+
+const ProductController = require("../controllers/product.controller");
+
+router.get('/', ProductController.daftarProduk)
+router.get('/:id', ProductController.getById)
+router.post(
+  '/tambahProduk',
+  (req, res, next) => {
+    const errors = [];
+    if (!req.body.nama) {
+      errors.push("Nama required");
+    }
+    if (!req.body.deskripsi) {
+      errors.push("Deskripsi required");
+    }
+    if (!req.body.harga) {
+      errors.push("Harga required");
+    }
+    else if (!req.body.image_url) {
+      errors.push("Image required");
+    }
+
+    if (errors.length > 0) {
+      next({
+        status: 400,
+        message: errors,
+      });
+    } else {
+      next();
+    }
+  },
+
+  ProductController.tambahProduk
+)
+
+module.exports = router;
